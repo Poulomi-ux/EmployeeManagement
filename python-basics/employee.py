569eb581c7d96f62d87a8aceb788786b24c88672
Poulomi Mukherjee <pmukherjee@geniusconsultant.com>
2026-09-10 18:53:37 +0530

    FirstDayCommit

---
+++ python-basics/employee.py
@@ -0,0 +1,59 @@
+# employee_name ="Arjun Das"
+# employee_email ="abc@gmail.com"
+# employee_age = 25
+
+# print(employee_name)
+# print(employee_email)
+# print(employee_age)
+
+
+# CREATE AN EMPLOYEE
+
+# name ="Riya Roy"
+# age = 30
+# salary = 30000
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Salary", salary)
+
+# CONDITIONS
+
+# name ="Riya Roy"
+# is_active = True
+
+# if is_active:
+#     print(name +" " +"is Active");
+# else:
+#     print("Employee is not Active");
+
+# USE f-string 
+
+# name ="Riya Roy"
+# is_active = True
+
+# if is_active:
+#     print(f"{name} is Active");
+# else:
+#     print("Employee is not Active");
+
+
+# USE FUNCTIONS
+
+# def display_employee(name, salary):
+#     print("Employee:", name)
+#     print("Salary:", salary)
+
+
+# display_employee("Arjun Das", 55000)
+# display_employee("Rahul Sen", 60000)
+
+# List
+
+employees =["Arjun das", "Riya Roy", "Rahul Sharma", "Amit Nag"]
+
+# print(employees)
+# print(employees[0])
+
+for employee in employees:
+    print(employee[3])
---
+++ python-basics/hello.py
@@ -0,0 +1,2 @@
+print("Hello");
+print("Employee Management");
